af1eed4093097733ee390210406ef5e2d4783c18
gwOURMS <118728590+gwOURMS@users.noreply.github.com>
2024-02-20 18:15:29 +0100

    début liste

--- P2I/liste.php
+++ P2I/liste.php
@@ -1,3 +1,41 @@
-<img src="<?= $film['Image2'] ?>" alt="Film Image"> 
+<?php 
+include("include/connect.php"); 
+$bdd = getDb();
 
-<input id='button' type=submit value="Ajouter">
+$query = "SELECT Titre, Image2 FROM films WHERE code = 1"; 
+$statement = $bdd->prepare($query);
+$statement->execute();
+$film = $statement->fetch(PDO::FETCH_ASSOC);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OzuSquare</title>
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+    <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+<img id="filmImage" src="<?= $film['Image2'] ?>" alt="Film Image"> 
+<input id="button" type="button" value="Ajouter à une liste">
+<ul id="listeFilms"></ul>
+
+<script> document.getElementById('button').addEventListener('click', function() {
+   
+    var imageUrl = document.getElementById('filmImage').src;
+
+    var newImage = document.createElement('img');
+    newImage.src = imageUrl;
+    newImage.alt = 'Film Image';
+
+    var listItem = document.createElement('li');
+    listItem.appendChild(newImage);
+
+    document.getElementById('listeFilms').appendChild(listItem);
+}); </script>
+
+</body>
+</html>
