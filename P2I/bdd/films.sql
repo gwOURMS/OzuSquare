dc1d403e3d19308386dc8cef5b6144e2e65f666d
gwOURMS <118728590+gwOURMS@users.noreply.github.com>
2024-04-05 11:23:55 +0200

    rendu final

--- P2I/bdd/films.sql
+++ P2I/bdd/films.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 07 fév. 2024 à 21:38
+-- Généré le : jeu. 07 mars 2024 à 00:17
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -44,7 +44,8 @@ CREATE TABLE `films` (
 --
 
 INSERT INTO `films` (`Titre`, `Réalisateur`, `Date`, `Synopsis`, `Affiche`, `Image1`, `Image2`, `Image3`, `Code`) VALUES
-('Irma Vep', 'Olivier Assayas', '1996', 'La grande star du cinéma asiatique Maggie Cheung débarque à Paris pour reprendre le rôle d\'Irma Vep dans un remake des Vampires, la célèbre série réalisée par Louis Feuillade entre 1915 et 1916, rôle qu\'avait incarné à l\'époque Musidora. En effet, René Vidal, le réalisateur, hanté par le fantôme de Musidora, voit en Maggie la seule actrice digne de reprendre son rôle et de donner vie à une Irma Vep moderne, malgré les problèmes de communications, Maggie Cheung ne parlant pas français et l\'équipe baragouinant en anglais...', 'images/IVaffiche.jpg', 'images/IVpluie.jpg', 'images/IVnavet.jpg', 'images/IVsourire.jpg', 1);
+('Irma Vep', 'Olivier Assayas', '1996', 'La grande star du cinéma asiatique Maggie Cheung débarque à Paris pour reprendre le rôle d\'Irma Vep dans un remake des Vampires, la célèbre série réalisée par Louis Feuillade entre 1915 et 1916, rôle qu\'avait incarné à l\'époque Musidora. En effet, René Vidal, le réalisateur, hanté par le fantôme de Musidora, voit en Maggie la seule actrice digne de reprendre son rôle et de donner vie à une Irma Vep moderne, malgré les problèmes de communications, Maggie Cheung ne parlant pas français et l\'équipe baragouinant en anglais...', 'images/IVaffiche.jpg', 'images/IVpluie.jpg', 'images/IVnavet.jpg', 'images/IVsourire.jpg', 1),
+('L\'Impossible Monsieur Bébé', 'Howard Hawks', '1938', 'La rencontre d’une jeune femme du monde, d’un léopard apprivoisé et d’un paléontologue à la recherche d’un os de dinosaure.', 'images/MBaffiche.jpg', 'images/MBbebe.jpg', 'images/MBrobe.jpg', 'images/MBrisque.jpg', 2);
 
 --
 -- Index pour les tables déchargées
@@ -64,7 +65,7 @@ ALTER TABLE `films`
 -- AUTO_INCREMENT pour la table `films`
 --
 ALTER TABLE `films`
-  MODIFY `Code` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Code` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
